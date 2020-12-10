90780fcb28ef5b2011a0746787841339689330d6
Yongzhe Huang <jk.aristode@gmail.com>
2020-12-10 01:25:55 -0500

    change out stats format to be easier adpated to stats collect script

--- src/KSplitStatsCollector.cpp
+++ src/KSplitStatsCollector.cpp
@@ -95,7 +95,8 @@ void pdg::KSplitStatsCollector::PrintKernelIdiomSharedStats()
   kernel_idiom_shared_stats_file << "num of handled array: " << num_of_handled_array_ << "\n";
   kernel_idiom_shared_stats_file << "num of unhandled array: " << num_of_unhandled_array_ << "\n";
   kernel_idiom_shared_stats_file << "num of string: " << num_of_string_op_ << "\n";
-  kernel_idiom_shared_stats_file << "num of void pointer/unhandled: " << num_of_void_pointer_op_ << "[" << num_of_unhandled_void_pointer_op_ << "]" << "\n";
+  kernel_idiom_shared_stats_file << "num of void pointer: " << num_of_void_pointer_op_ << "\n";
+  kernel_idiom_shared_stats_file << "num of unhandled void pointer: " <<  num_of_unhandled_void_pointer_op_ << "\n";
   kernel_idiom_shared_stats_file << "num of container_of operation: " << num_of_container_of_macro_ << "\n";
   kernel_idiom_shared_stats_file << "num of union type data: " << num_of_union_op_ << "\n";
   kernel_idiom_shared_stats_file << "num of unsafe type cast: " << num_of_unsafe_casted_struct_pointer_ << "\n";
